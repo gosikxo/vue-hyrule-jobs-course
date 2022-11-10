67a5a36e40a76556bfbd9b8733699b8ca452a85e
Gosia Brozek <gosikbrozek1997@gmail.com>
2022-11-10 17:30:42 +0100

    Add JobList component

---
+++ src/components/JobList.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="job-list">
+        <ul>
+            <li v-for="job in jobs" :key="job.id">
+                <h2>{{ job.title }} in {{ job.location }}</h2>
+                <div class="salary">
+                    <p>{{ job.salary }} rupees</p>
+                </div>
+                <div class="description">
+                    <p>Doggo ipsum doggo extremely cuuuuuute puggo sub woofer very good spot he made many woofs, boofers
+                        floofs blep such treat adorable doggo, super chub bork corgo doge. Length boy waggy wags long
+                        water shoob heckin angery woofer shibe snoot extremely cuuuuuute, borkdrive doing me a frighten
+                        long bois ur givin me a spook.</p>
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import Job from '@/types/Job'
+
+export default defineComponent({
+    props: {
+        jobs: {
+            required: true,
+            type: Array as PropType<Job[]>
+        }
+    }
+})
+</script>
+
+<style scoped>
+.job-list {
+    max-width: 960px;
+    margin: 40px auto;
+}
+
+.job-list ul {
+    padding: 0;
+}
+
+.job-list li {
+    list-style-type: none;
+    background: white;
+    padding: 16px;
+    margin: 16px 0;
+    border-radius: 4px;
+}
+
+.job-list h2 {
+    margin: 0 0 10px;
+    text-transform: capitalize;
+}
+
+.salary {
+    display: flex;
+}
+
+.salary img {
+    width: 30px;
+}
+
+.salary p {
+    color: #17bf66;
+    font-weight: bold;
+    margin: 10px 4px;
+}
+</style>
